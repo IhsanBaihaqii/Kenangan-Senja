95fb73906773adf6e44f6386acee2c06aba31951
IhsanBaihaqii <baihaqiihsan31@gmail.com>
2025-06-17 20:39:51 +0700

    Membuat Keranjang

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-// toggle class active hamburger
+// setup
 const navbar = document.querySelector(".navbar");
-//di klik
-document.querySelector("#hamburger").onclick = () => {
+const hamburger = document.querySelector("#hamburger");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchBtn = document.querySelector("#search-btn");
+const shoopingBtn = document.querySelector("#shooping-btn");
+const shoopingCart = document.querySelector(".shopping");
+
+//toggle class active hamburger
+hamburger.onclick = () => {
   navbar.classList.toggle("active");
 };
 
 // toggle class active search
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
-const searchBtn = document.querySelector("#search-btn");
 searchBtn.onclick = (e) => {
   searchForm.classList.toggle("active");
   searchBox.focus();
   e.preventDefault(); //Mematikan aksi link pada href
 };
 
+// toggle class active shooping
+shoopingBtn.onclick = (e) => {
+  shoopingCart.classList.toggle("active");
+  e.preventDefault(); //Mematikan aksi link pada href
+};
+
 // klik diluar elemen
-const hamburger = document.querySelector("#hamburger");
 document.addEventListener("click", function (e) {
   //navbar
   if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
@@ -25,5 +34,9 @@ document.addEventListener("click", function (e) {
   //search button
   if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  //Shooping
+  if (!shoopingBtn.contains(e.target) && !shoopingCart.contains(e.target)) {
+    shoopingCart.classList.remove("active");
   }
 });
