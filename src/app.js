b273e59774d6b5f8be091a01fcceb66a431a9c70
IhsanBaihaqii <baihaqiihsan31@gmail.com>
2025-07-01 20:05:16 +0700

    Menambah / Menghapus Item Keranjang

--- src/app.js
+++ src/app.js
@@ -14,10 +14,55 @@ document.addEventListener("alpine:init", () => {
     total: 0,
     quantity: 0,
     add(getItem) {
-      this.items.push(getItem);
-      this.quantity++;
-      this.total = +this.price;
-      console.log(this.quantity);
+      // Check barang yang sama
+      cartItem = this.items.find((item) => item.id === getItem.id);
+      // Jika tidak ada barang yang sama / masih kosong
+      if (!cartItem) {
+        this.items.push({ ...getItem, quantity: 1, total: getItem.price });
+        this.quantity++;
+        this.total += getItem.price;
+      } else {
+        // Jika barang ada yang sama
+        this.items = this.items.map((item) => {
+          // Barang berbeda
+          if (item.id !== getItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambahkan quantity dan total
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      console.log(id);
+      // ambil item berdasarkan id
+      cartItem = this.items.find((item) => item.id === id);
+      //  jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusuri 1/1
+        this.items = this.items.map((item) => {
+          // jika bukan barang yg di klik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // Jika barang sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
     },
   });
 });
